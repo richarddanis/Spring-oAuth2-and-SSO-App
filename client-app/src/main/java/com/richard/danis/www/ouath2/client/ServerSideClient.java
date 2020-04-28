71bd7f28448e9b4d472ef736698b3abfc76c8740
Richard Danis <Richard_Danis@epam.com>
2020-04-28 14:38:52 +0200

    update project to spring sec 5.0

--- client-app/src/main/java/com/richard/danis/www/ouath2/client/ServerSideClient.java
+++
@@ -1,9 +0,0 @@
-package com.richard.danis.www.ouath2.client;
-
-import retrofit2.Call;
-import retrofit2.http.GET;
-
-public interface ServerSideClient {
-    @GET("/hello")
-    Call<String> hello();
-}
